d5cbccc1e36f3bf79c13ccf74cbe9bf440fe02f5
Michael Yingling <myingling33@gmail.com>
2024-03-25 13:01:40 -0500

    Practiced adding beans

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,9 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age, Address address) {};
+record Address(String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -10,4 +13,35 @@ public class HelloWorldConfiguration {
     public String name() {
         return "Ranga";
     }
+
+    @Bean
+    public int age() {
+        return 15;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name, age, address3);
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        return new Address("Baker Street", "London");
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("Motinagar", "Hyderabad");
+    }
+
 }
