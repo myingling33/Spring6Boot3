ce519bd3c99a2920d561042388d2614e399b2e44
Michael Yingling <myingling33@gmail.com>
2024-03-29 13:34:39 -0500

    queried data using Spring JDBC

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -16,6 +16,11 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
         repository.insert(new Course(1, "Learn AWS Now!", "in28minutes"));
         repository.insert(new Course(2, "Learn Azure Now!", "in28minutes"));
         repository.insert(new Course(3, "Learn Devops Now!", "in28minutes"));
+
         repository.deleteById(1);
+
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
+
     }
 }
