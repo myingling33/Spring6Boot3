d3a713a541368361a49455a7b542599324f11e58
Michael Yingling <myingling33@gmail.com>
2024-03-29 15:06:16 -0500

    Spring Data JPA

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,6 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course;
 
-import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,19 +11,31 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS Jpa!", "in28minutes"));
-        repository.insert(new Course(2, "Learn Azure Jpa!", "in28minutes"));
-        repository.insert(new Course(3, "Learn Devops Jpa!", "in28minutes"));
+        repository.save(new Course(1, "Learn AWS Jpa!", "in28minutes"));
+        repository.save(new Course(2, "Learn Azure Jpa!", "in28minutes"));
+        repository.save(new Course(3, "Learn Devops Jpa!", "in28minutes"));
 
-        repository.deleteById(1);
+        repository.deleteById(1L);
 
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        System.out.println(repository.findById(2L));
+        System.out.println(repository.findById(3L));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        System.out.println(repository.findByAuthor("in28minutes"));
+        System.out.println(repository.findByAuthor(""));
+
+        System.out.println(repository.findByName("Learn AWS Jpa!"));
+        System.out.println(repository.findByName("Learn Devops Jpa!"));
 
     }
 }
